341da646776c73086bb4cb6a347551dacaee24d7
Rueie <darling_rueie@mail.ru>
2024-08-20 18:17:05 +0300

    Подготовка к protobuf & kafka работе, part 3

--- service/account/build_proto.sh
+++ service/account/build_proto.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#protoc --go_out=./proto  --go-grpc_out=./proto  -I ./proto  ./proto/platform/platform.proto
+protoc --go_out=./proto  --go-grpc_out=./proto  -I ./proto  ./proto/platform/platform.proto
 protoc --go_out=./proto  --go-grpc_out=./proto  -I ./proto  ./proto/account/account.proto
