aafcead787a7cb2f5bd003679b550e274a9e655c
rueie <darling_rueie@mail.ru>
2024-11-10 13:45:26 +0300

    Work on grpc

--- service/registration/internal/registration/grpc/grpc_handlers.go
+++ service/registration/internal/registration/grpc/grpc_handlers.go
@@ -1,1 +1,50 @@
 package grpc
+
+import (
+	"context"
+	"github.com/GCFactory/dbo-system/platform/pkg/logger"
+	"github.com/GCFactory/dbo-system/service/registration/config"
+	"github.com/GCFactory/dbo-system/service/registration/internal/registration"
+	"github.com/GCFactory/dbo-system/service/registration/internal/registration/usecase"
+	"github.com/GCFactory/dbo-system/service/registration/pkg/kafka"
+)
+
+type RegistrationHandlers struct {
+	cfg            *config.Config
+	kProducer      *kafka.ProducerProvider
+	registrationUC registration.UseCase
+	accLog         logger.Logger
+}
+
+func (h *RegistrationHandlers) StartOperation(ctx context.Context, operation_type uint8, operation_data []byte) (err error) {
+	// TODO: complete
+
+	switch operation_type {
+	case usecase.OperationCreateUser:
+		{
+			list_of_events, err := h.registrationUC.StartOperation(ctx, operation_type)
+			if err != nil {
+				return err
+			}
+			for _, event := range list_of_events {
+				
+			}
+
+		}
+	default:
+		{
+			h.accLog.Debug("Unknown type of operation!")
+		}
+	}
+
+	return nil
+}
+
+func (h *RegistrationHandlers) Process(ctx context.Context, event_type string, event_data []byte) (err error) {
+	// TODO: complete
+	return nil
+}
+
+func NewRegistrationGRPCHandlers(cfg *config.Config, kProducer *kafka.ProducerProvider, registrationUC registration.UseCase, accLog logger.Logger) registration.RegistrationGRPCHandlers {
+	return &RegistrationHandlers{cfg: cfg, kProducer: kProducer, registrationUC: registrationUC, accLog: accLog}
+}
--- service/registration/internal/registration/grpc_handlers.go
+++ service/registration/internal/registration/grpc_handlers.go
@@ -1,1 +1,10 @@
 package registration
+
+import (
+	"context"
+)
+
+type RegistrationGRPCHandlers interface {
+	StartOperation(ctx context.Context, operation_type uint8, operation_data []byte) error
+	Process(ctx context.Context, event_type string, event_data []byte) error
+}
